686e56eb850a2542631b3f4be04d040e50a17f70
Michael Denicore <mdenicore@LL-MAC-006.local>
2022-06-06 16:53:03 -0400

    aws updates

---
+++ src/components/timeline.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Lead Software Engineer at LifeLens Technologies<span>&nbsp;2020-present</span></h2>
+                        <p>Leading the design and delivery of medical software SPA and mobile applications utilizing 
+                          Java, Kotlin, React/NodeJS and REST SOA. Mentoring Junior Software Engineers in the design and delivery of 
+                          React, NodeJS, and Android applications.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Principal Application Architect at ADP<span>&nbsp;2011-2020</span></h2>
+                        <p>Providing the technical design and development of enterprise applications to support the
+migration, integration, and implementation of over 120k clients on ADP’s SOA.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Development Manager at ADP<span>&nbsp;2005-2011</span></h2>
+                        <p>Development manager for team of 20+ local and offshore developers delivering J2EE-based
+Web2.0 HCM solutions on ADP’s Portal.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Senior Developer at ADP<span>&nbsp;2002-2005</span></h2>
+                        <p>Full-stack solution developer responsible for delivering Java EE applications providing a PEO
+solution for clients to manage hr, benefits, and payroll.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Solution Developer at Enterpulse<span>&nbsp;2000-2002</span></h2>
+                        <p>Consultant in the design and development of custom web and client-server solutions utilizing
+various technologies for clients including www.bankersbank.com and www.bancintelligence.com.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
